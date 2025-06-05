f4fb1750f9308020d23ddc702bf1f0100bc8206a
datageni <abrahamjmlicona@gmail.com>
2025-06-05 15:08:57 -0600

    configure api service

--- api.py
+++ api.py
@@ -585,4 +585,7 @@ def root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000) 
+    import os
+
+    port = int(os.environ.get("PORT", 8000))  # Usa el puerto de Render si existe
+    uvicorn.run(app, host="0.0.0.0", port=port)
